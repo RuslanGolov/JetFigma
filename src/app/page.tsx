821b999a7ff883d1a4400f34918c92ded4747e4f
ruslan.golov <ruslan.golov@jetbrains.com>
2026-01-20 18:37:20 +0100

    test

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { HeroDemo } from "@/components/ui/demo";
+import { HeroScrollDemo } from "@/components/ui/hero-scroll-demo";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroDemo />
+      <HeroScrollDemo />
+    </main>
+  );
+}
